7e8ba93dcd6343f6e8eb63b6d3c6c1f7f5379f38
deepdivesecurity <165413290+deepdivesecurity@users.noreply.github.com>
2026-08-04 20:25:18 -0400

    screen and initial snake setup

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from turtle import Screen, Turtle
+
+def setup_screen(): 
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake")
+    return screen
+
+def create_snake(): 
+    count = 0
+    for _ in range(3): 
+        snake_block = Turtle("square")
+        snake_block.color("white")
+        snake_block.goto(count, 0)
+        count -= 20
+
+def main(): 
+    screen = setup_screen()
+    create_snake()
+    screen.exitonclick()
+
 if __name__ == "__main__":
-    print("Hello, world")
+    main()
