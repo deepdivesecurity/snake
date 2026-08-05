ad7cc1f6033f3c0af9b41daa7ecee680aefcf481
deepdivesecurity <165413290+deepdivesecurity@users.noreply.github.com>
2026-08-05 09:22:26 -0400

    Adding boundary check for snake

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from snake import Snake
 import time
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
 def setup_screen(): 
     screen = Screen()
-    screen.setup(width=600, height=600)
+    screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
     screen.bgcolor("black")
     screen.title("Snake")
     screen.tracer(0)
@@ -20,11 +23,20 @@ def main():
     screen.onkey(snake.left, "Left")
     screen.onkey(snake.right, "Right")
 
+    error_message = Turtle()
+    error_message.hideturtle()
+    error_message.color("red")
+    error_message.penup()
+
     game_on = True
     while game_on:
         screen.update()
         time.sleep(0.1)
         snake.move()
+        if snake.head.xcor() > SCREEN_WIDTH / 2 or snake.head.xcor() < SCREEN_WIDTH / -2 or snake.head.ycor() > SCREEN_HEIGHT / 2 or snake.head.ycor() < SCREEN_HEIGHT / -2: 
+            error_message.goto(0, 0)
+            error_message.write("Game Over", align="center", font=("Arial", 24, "normal"))
+            game_on = False
 
     screen.exitonclick()
 
